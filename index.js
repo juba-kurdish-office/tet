d49ed7dbb0a34c9ff5591ee22e94304c99f8882c
Pranav Tyagi <85784258+Pranav6966@users.noreply.github.com>
2021-09-06 19:50:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+require("dotenv").config({ path: "src/.env" });
+
+const fs = require("fs");
+const chalk = require("chalk");
+
+const { Client, Collection, Intents, MessageEmbed } = require("discord.js");
+const { DEFAULT_PREFIX, BOT_TOKEN, ERROR_LOGS_CHANNEL } = require("./config.json");
+const { loadCommands } = require("./handler/loadCommands");
+const { loadEvents } = require("./handler/loadEvents");
+const { loadSlashCommands } = require("./handler/loadSlashCommands")
+const { DiscordTogether } = require('discord-together')
+
+const client = new Client({
+  allowedMentions: { parse: ["users", "roles"] },
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_BANS,
+    //Intents.FLAGS.GUILD_PRESENCES,
+  ],
+});
+
+client.discordTogether = new DiscordTogether(client);
+client.commands = new Collection();
+client.slash = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./Commands/");
+client.setMaxListeners(0);
+
+loadCommands(client);
+loadEvents(client);
+loadSlashCommands(client);
+
+
+// Error Handling
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception: " + err);
+
+  const exceptionembed = new MessageEmbed()
+  .setTitle("Uncaught Exception")
+  .setDescription(`${err}`)
+  .setColor("RED")
+  client.channels.cache.get(ERROR_LOGS_CHANNEL).send({ embeds: [exceptionembed] })
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log(
+    "[FATAL] Possibly Unhandled Rejection at: Promise ",
+    promise,
+    " reason: ",
+    reason.message
+  );
+
+   const rejectionembed = new MessageEmbed()
+  .setTitle("Unhandled Promise Rejection")
+  .addField("Promise", `${promise}`)
+  .addField("Reason", `${reason.message}`)
+  .setColor("RED")
+  client.channels.cache.get(ERROR_LOGS_CHANNEL).send({ embeds: [rejectionembed] })
+});
+
+client.login(BOT_TOKEN).then(() => {
+  console.log(
+    chalk.bgBlueBright.black(
+      ` Successfully logged in as: ${client.user.username}#${client.user.discriminator} `
+    )
+  );
+});
